3a1eaf88967c1422f23272455824e4abed0cff5c
Masha <39933741+Marechka@users.noreply.github.com>
2022-09-13 09:26:18 -0700

    Added styled component "Button" & "FormControl"

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,46 @@
 import React, { useState } from "react";
-
+import styled from 'styled-components'
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+//import "./CourseInput.css";
+
+
+const FormControl = styled.div`
+  
+    margin: 0.5rem 0;
+  
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transperent')}
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+  // can remove this style with using props as well 
+  // &.invalid input {
+  //   border-color: red;
+  //   background: rgb(235, 152, 152);
+  // }
+
+  // we can remove this style now. color moved to the 'label', since we make a style definition on valid/invalid there
+  // &.invalid label {
+  //   color: red;
+  // }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -25,18 +64,26 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* replacing div with a new styled component */}
+      {/* passing css class, better than inline style
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         {/* can use inline styling for dynamic change */}
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        {/* <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           style={{
             borderColor: !isValid ? "red" : "black",
             background: !isValid ? "salmon" : "transperent",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+          }} */}
+        {/* we could do validation inside styled component */}
+        {/* <FormControl className={!isValid && 'invalid'}> */}
+        {/* or we could use props and have function in styles of FormControl */}
+        <FormControl invalid={!isValid}>
+          <label >Course Goal</label>
+          <input 
+          type="text" onChange={goalInputChangeHandler}
+          />
+        </FormControl>
+      {/* </div> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
